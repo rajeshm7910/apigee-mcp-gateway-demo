86ab05d1a6206db32c2f96b768a33ecc205fb48b
Rajesh Mishra <rajeshmi@google.com>
2025-06-09 12:47:49 -0700

    Proxy name env variable

--- mcp-server/apigee-mcp-server/server.py
+++ mcp-server/apigee-mcp-server/server.py
@@ -11,8 +11,10 @@ load_dotenv()
 base_url = os.getenv("PROXY_BASE_URL", "https://bap-amer-west-demo1.cs.apigee.net/retail/v1")
 client = httpx.AsyncClient(base_url=base_url)
 
+proxy_name = os.getenv("PROXY_NAME", "retail-v1");
+
 # Load your OpenAPI spec 
-openapi_spec =  create_openapi_spec("retail-v1")
+openapi_spec =  create_openapi_spec(proxy_name)
 
 # Create the MCP server
 mcp = FastMCP.from_openapi(
